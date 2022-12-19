fb346d8441fe35885793ef772a0dac638f4471d5
Davy <contato.davygabriel@gmail.com>
2022-12-19 14:34:52 -0300

    Escopo do projeto

--- ClienteController/Controllers/ClienteController.cs
+++ ClienteController/Controllers/ClienteController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ClienteController.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ClienteController.Controllers
@@ -7,5 +8,36 @@ namespace ClienteController.Controllers
     [ApiController]
     public class ClienteController : ControllerBase
     {
+        public static List<Cliente> clientes = new List<Cliente>();
+        [HttpPost("Cadastrar")]
+        public void Cadastrar([FromBody] Cliente cliente)
+        {
+
+        }
+
+        [HttpGet("Listar")]
+        public List<Cliente> Listar()
+        {
+
+        }
+
+        [HttpGet("Buscar por CPF")]
+        public List<Cliente> BuscaPorCpf()
+        {
+
+        }
+
+        [HttpPut("Alteração de Cadastro")]
+        public void AlteracaoDeCadastro()
+        {
+
+        }
+
+        [HttpDelete("Deletar Cadastro")]
+        public void Deletar()
+        {
+
+        }
+
     }
 }
---
+++ ClienteController/Models/Idade.cs
@@ -0,0 +1,9 @@
+﻿namespace ClienteController.Models
+{
+    public class Idade
+    {
+        public int Dia { get; set; }    
+        public int Mes { get; set; }
+        public int Ano { get; set; }
+    }
+}
