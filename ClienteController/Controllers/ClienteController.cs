02f343a9ec3d5516eb3fb51993860fe2fe5d3393
Davy <contato.davygabriel@gmail.com>
2022-12-20 11:21:12 -0300

    Lógica CRUD do negócio

--- ClienteController/Controllers/ClienteController.cs
+++ ClienteController/Controllers/ClienteController.cs
@@ -1,6 +1,7 @@
 ﻿using ClienteController.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
 
 namespace ClienteController.Controllers
 {
@@ -9,35 +10,70 @@ namespace ClienteController.Controllers
     public class ClienteController : ControllerBase
     {
         public static List<Cliente> clientes = new List<Cliente>();
-        [HttpPost("Cadastrar")]
-        public void Cadastrar([FromBody] Cliente cliente)
-        {
 
+        [HttpPost("Cadastrar")]
+        public IActionResult Cadastrar([FromBody] Cliente cliente)
+        {
+            var usuario = new Cliente(cliente.Nome, cliente.DataNascimento, cliente.Email, cliente.Cpf);
+
+            //regras do negócio:
+            if (!usuario.ValidacaoIdade())
+                return UnprocessableEntity("Menor de idade não cria conta");
+            if(!usuario.ValidacaoNome())
+                return UnprocessableEntity("O nome pode ter de 1 a 255 caracteres");
+            if (!usuario.ValidacaoEmail())
+                return UnprocessableEntity("O e-mail para ser válido precisa contem @ e .com");
+            if (!usuario.ValidacaoCpf())
+                return UnprocessableEntity("O cpf precisa conter 11 caracteres");
+
+            clientes.Add(usuario);
+            return Created("", usuario);
         }
 
         [HttpGet("Listar")]
         public List<Cliente> Listar()
         {
-
+            return clientes;
         }
 
         [HttpGet("Buscar por CPF")]
-        public List<Cliente> BuscaPorCpf()
+        public Cliente BuscaPorCpf(string cpf)
         {
-
+            for(int i = 0; i < clientes.Count; i++)
+            {
+                if(clientes[i].Cpf == cpf)
+                {
+                    return clientes[i];
+                }
+            }
+            return null;
         }
 
         [HttpPut("Alteração de Cadastro")]
-        public void AlteracaoDeCadastro()
+        public void AlteracaoDeCadastro(string cpf, [FromBody] Cliente cliente)
         {
-
+            for (int i = 0; i < clientes.Count; i++)
+            {
+                if (clientes[i].Cpf == cpf)
+                {
+                    clientes[i].Email = cliente.Email;
+                    clientes[i].Cpf = cliente.Cpf;
+                    clientes[i].Nome = cliente.Nome;
+                    clientes[i].DataNascimento = cliente.DataNascimento;
+                }
+            }
         }
 
         [HttpDelete("Deletar Cadastro")]
-        public void Deletar()
+        public void Deletar(string cpf)
         {
-
+            for (int i = 0; i < clientes.Count; i++)
+            {
+                if (clientes[i].Cpf == cpf)
+                {
+                    clientes.Remove(clientes[i]);
+                }
+            }
         }
-
     }
 }
--- ClienteController/Models/Cliente.cs
+++ ClienteController/Models/Cliente.cs
@@ -1,10 +1,41 @@
-﻿namespace ClienteController.Models
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace ClienteController.Models
 {
     public class Cliente
-    { 
+    {
         public string Nome { get; set; }
         public Idade DataNascimento { get; set; }
         public string Email { get; set; }
-        public string Cpf { get; set; } 
+        public string Cpf { get; set; }
+        public Cliente(string nome, Idade dataNascimento, string email, string cpf)
+        {
+            Nome = nome;
+            DataNascimento = dataNascimento;
+            Email = email;
+            Cpf = cpf;
+        }
+
+        public bool ValidacaoNome()
+        {
+
+        }
+
+        public bool ValidacaoIdade()
+        {
+
+        }
+
+        public bool ValidacaoEmail()
+        {
+
+        }
+
+        public bool ValidacaoCpf()
+        {
+
+        }
+
+
     }
 }
