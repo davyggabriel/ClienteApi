b6bae93e0b57896876c855977944826d82da7772
Davy <contato.davygabriel@gmail.com>
2022-12-20 11:27:44 -0300

    Correção de erros

--- ClienteController/Models/Cliente.cs
+++ ClienteController/Models/Cliente.cs
@@ -3,7 +3,6 @@
 namespace ClienteController.Models
 {
     public class Cliente
-<<<<<<< HEAD
     {
         public string Nome { get; set; }
         public Idade DataNascimento { get; set; }
@@ -36,19 +35,5 @@ namespace ClienteController.Models
         {
 
         }
-
-
-=======
-<<<<<<< HEAD
-    { 
-        public string Nome { get; set; }
-        public Idade DataNascimento { get; set; }
-        public string Email { get; set; }
-        public string Cpf { get; set; } 
-=======
-    {
-        //oi
->>>>>>> af9789f5b1cba3ab39f6d9eebcb9bf6798ac508f
->>>>>>> fd7967d0b97f76e98e7ab280090c0a8c5fb07ff6
     }
 }
